ee0f53019806e6f791b16f569663a9c762af4d77
Rui Kowase <rui.kowase@gmail.com>
2017-12-03 01:51:37 +0900

    Implement feature of listing repositories

--- app/src/main/java/rkowase/mvpsample/MainActivity.kt
+++ app/src/main/java/rkowase/mvpsample/MainActivity.kt
@@ -2,11 +2,60 @@ package rkowase.mvpsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val service = createService()
+
+        button.setOnClickListener({
+            service.listRepos(getString(R.string.user))
+                    .subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        showList(it)
+                        hideButton()
+                    }, {
+                        showErrorMessage()
+                    })
+        })
+    }
+
+    private fun createService(): GitHubService {
+        val retrofit = Retrofit.Builder()
+                .baseUrl(getString(R.string.base_url))
+                .addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+
+        return retrofit.create(GitHubService::class.java)
+    }
+
+    private fun hideButton() {
+        button.visibility = View.GONE
+    }
+
+    private fun showErrorMessage() {
+        Toast.makeText(this, getString(R.string.error_message), Toast.LENGTH_LONG).show()
+    }
+
+    private fun showList(it: List<RepoEntity>) {
+        var list = listOf<String>()
+        it.forEach { list += it.name }
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list)
+        listView.adapter = adapter
     }
 }
