ad5498446960264dd2bea39bba96827e8ef761f4
Rui Kowase <rui.kowase@gmail.com>
2017-12-05 00:10:23 +0900

    Add request method

--- app/src/main/java/rkowase/mvpsample/data/repository/GitHubRepositoryImpl.kt
+++ app/src/main/java/rkowase/mvpsample/data/repository/GitHubRepositoryImpl.kt
@@ -1,20 +1,26 @@
 package rkowase.mvpsample.data.repository
 
 import android.content.Context
+import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
-import rkowase.mvpsample.GitHubService
 import rkowase.mvpsample.R
+import rkowase.mvpsample.data.entity.RepoEntity
+import rkowase.mvpsample.service.GitHubService
 
 class GitHubRepositoryImpl(private val context: Context): GitHubRepository {
-    override fun createService(): GitHubService {
+    private lateinit var mService: GitHubService
+
+    override fun initService() {
         val retrofit = Retrofit.Builder()
                 .baseUrl(context.getString(R.string.base_url))
                 .addConverterFactory(MoshiConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
 
-        return retrofit.create(GitHubService::class.java)
+        mService = retrofit.create(GitHubService::class.java)
     }
+
+    override fun request(user: String): Observable<List<RepoEntity>> = mService.listRepos(user)
 }
